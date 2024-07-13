c44aedb5ed5a5e4b939202414ec7d333b7ea956e
walter.hwang <lyvius2@naver.com>
2024-07-14 00:26:00 +0900

    initial commit

---
+++ src/main/kotlin/com/walter/jdsl/JdslReferenceApplication.kt
@@ -0,0 +1,11 @@
+package com.walter.jdsl
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JdslReferenceApplication
+
+fun main(args: Array<String>) {
+    runApplication<JdslReferenceApplication>(*args)
+}
